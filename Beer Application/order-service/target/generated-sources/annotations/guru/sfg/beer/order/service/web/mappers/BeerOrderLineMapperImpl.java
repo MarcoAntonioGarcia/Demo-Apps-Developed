8f36c85340380288d8752a7cf35b58c73fb5c975
MarcoAntonioGarcia <70038063+MarcoAntonioGarcia@users.noreply.github.com>
2022-05-24 01:07:44 -0500

    Add files via upload

---
+++ Beer Application/order-service/target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,28 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.brewery.model.BeerOrderLineDto;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-05-20T09:38:23-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 17 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerOrderLineMapper delegate;
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto)  {
+        return delegate.dtoToBeerOrderLine( dto );
+    }
+}
